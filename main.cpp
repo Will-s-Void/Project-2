f6a5a19c8bc25133a5d00ce64c33d564b8f034c7
Will0376 <lbvrf090@gmail.com>
2021-11-01 16:03:41 +0300

    инит

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    //Скриншот. https://cdn.discordapp.com/attachments/890216273445810186/890217188332535889/unknown.png
+    return 0;
+}
